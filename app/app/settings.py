4a2d29747b1851105357541ecabd5dd0244f5073
Mohamad Nour Ahmad <noor.ceng@gmail.com>
2021-08-13 16:18:37 +0100

    Corrected secret key path in settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #Read SECRET_KEY from secret_key.txt under "profiles_project folder"
 SECRET_KEY_FILE = 'secret_key.txt'
-SECRET_KEY_PATH = os.path.join('profiles_project', SECRET_KEY_FILE)
+SECRET_KEY_PATH = os.path.join('app', SECRET_KEY_FILE)
 with open(SECRET_KEY_PATH,'r') as f:
     SECRET_KEY = f.read().strip()
 
